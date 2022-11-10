dc834dcb23de1f2ab6c829ecdec1dbc6da73a117
Sud <sudarsana.ala@gmail.com>
2022-11-10 16:54:17 +0530

    Initial commit

---
+++ src/test/java/org/example/CalcTest.java
@@ -0,0 +1,21 @@
+package org.example;
+
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+
+/**
+ * Unit test for simple App.
+ */
+public class CalcTest
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void validateAdd()
+    {
+        int result = new Calculator().add(1, 2);
+        Assert.assertEquals(result, 3);
+    }
+}
